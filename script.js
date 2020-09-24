f9a4de276fd5e4cf314291d7172259dc844431c9
gokhan <gdogan3746@gmail.com>
2020-09-24 18:00:16 +0200

    Edit JS codes

--- script.js
+++ script.js
@@ -1,38 +1,41 @@
 const input = document.getElementById("input");
 const button = document.getElementById("btn");
-const resultt = document.getElementById("result")
-button.addEventListener("click", fibonacci);
+const resultt = document.getElementById("result");
+button.addEventListener("click", trigger);
+
+function trigger() {
+  if (input.value < 0 || input.value =="") {
+    resultt.innerHTML = "Check the number!!!";
+  } else {
+    fibonacci();
+  }
+}
 
 function fibonacci(){
 
     if(input.value == 0){
-        resultt.innerHTML = `${input.value} fibonacci value is 0. `}
+        resultt.innerHTML = `The Fibonacci value of ${input.value} is 0. `}
 
     else if(input.value == 1 || input.value == 2){
-        resultt.innerHTML = `${input.value} fibonacci value is 1. `}
+        resultt.innerHTML = `The Fibonacci value of ${input.value} is 1. `}
     else{
         fibonacci_series(input.value)[input.value]
-        resultt.innerHTML = `${input.value} fibonacci value is ${fibonacci_series(input.value)[input.value]}. `}
-    
-}
+        resultt.innerHTML = `The Fibonacci value of ${input.value} is ${fibonacci_series(input.value)[input.value]}. `}
+    }
 
-
-var fibonacci_series = function (n) 
+var fibonacci_series = function (n)
 {
-  if (n==1) 
+  if (n==1)
   {
     return [0, 1];
-  } 
-  else 
+  }
+  else
   {
     var s = fibonacci_series(n - 1);
     s.push(s[s.length - 1] + s[s.length - 2]);
-    
+
     return s;
-    
+
   }
 };
-// console.log(fibonacci_series(8));
- 
- 
-
+console.log(fibonacci_series(8));
